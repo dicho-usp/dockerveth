91f359814c4f71ed50f71538b9d1d3dd19915e65
Radoslav Georgiev <rgeorgiev583@gmail.com>
2023-03-30 13:52:50 +0300

    format code

--- veth.sh
+++ veth.sh
@@ -6,4 +6,4 @@ while read iface id; do
         [[ "$iface" == lo || ! "$iface" = eth* ]] && continue
         veth=$(ip -br addr | sed -nre "s/(veth.*)@if$id.*/\1/p")
         echo -e "$NAME\t$iface\t$veth"
-done < <(</proc/$PID/net/igmp awk '/^[0-9]+/{print $2 " " $1;}')
+done < <(awk </proc/$PID/net/igmp '/^[0-9]+/{print $2 " " $1;}')
